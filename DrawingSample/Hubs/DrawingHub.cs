539429b5b7238a0ca4983d7e251ee0f67b0579b3
Jordan Hamade <ardf2014@hotmail.com>
2017-04-12 23:19:32 -0700

    initial commit
    Signed-off-by: Jordan Hamade <ardf2014@hotmail.com>

---
+++ DrawingSample/Hubs/DrawingHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace DrawingSample.Hubs
+{
+    [HubName("drawingHub")]
+    public class DrawingHub : Hub
+    {
+        public void UpdateCanvas(int x, int y)
+        {
+            Clients.All.updateDot(x, y);
+        }
+
+        public void ClearAllCanvas()
+        {
+            Clients.All.clearAllCanvas();
+        }
+
+        public void DrawMove(int[] p, string color)
+        {
+            Clients.Others.drawMove(p, color);
+        }
+    }
+}
